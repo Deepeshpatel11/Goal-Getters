d97e07b13b79a81335582e1e6f76f3aa8c1ff979
Deepesh Patel <deepeshpatel28@icloud.com>
2024-08-15 13:19:29 +0000

    Added js functions to js page: empty functions that need to be developed still

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
-// Function to start the quiz
+// Global Variables for quiz
+
+
+/**
+ * Function to start the quiz from index.html 
+ */
 function startQuiz(category) {
     const username = document.getElementById('username').value;
     if (!username) {
@@ -8,4 +13,53 @@ function startQuiz(category) {
     localStorage.setItem('quizUsername', username); // Store username in localStorage
     localStorage.setItem('quizCategory', category); // Store category in localStorage
     window.location.href = 'quiz.html'; // Redirect to quiz page
+}
+
+/**
+ * Function to load questions and start the quix in quiz.html
+ */
+function loadQuestions() {
+
+}
+
+/**
+ * Function to load next question
+ */
+function loadNextQuestion() {
+
+}
+
+/**
+ * Function to check the user's answer and update the score
+ */
+function checkAnswer() {
+
+}
+
+/**
+ * Function to end the quiz and display the final score
+ */
+function endQuiz() {
+
+}
+
+/**
+ * Function to start the quiz timer
+ */
+function startTimer() {
+
+}
+
+/**
+ * Function to display the remaining time
+ */
+function displayTimer() {
+
+}
+
+/**
+ * Function to return to the main menu from quiz.html
+ */
+function returnToMainMenu() {
+
 }
